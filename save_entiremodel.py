39764ed1e2e4662412e674553b0ee76092bc665b
sangje <parksj2480@naver.com>
2023-02-10 01:18:02 +0900

    Error mod

--- save_entiremodel.py
+++ save_entiremodel.py
@@ -15,7 +15,7 @@ def make(args):
     #model = load_model(config).to(device)
     model = load_model(config)
     checkpoint = torch.load('checkpoints/mfa_1024_vox1/model.pt')
-    model.load_state_dict(checkpoint)
+    model.load_state_dict(checkpoint['model'],strict=False)
     torch.save(model,'model.pth')
 
 if __name__ == '__main__':
