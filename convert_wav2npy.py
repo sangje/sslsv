222a58793833cdcdf8c0f71e9f55a09ee6d082aa
sangje <parksj2480@naver.com>
2023-02-13 17:17:11 +0900

    Convert code modified using with open

--- convert_wav2npy.py
+++ convert_wav2npy.py
@@ -3,13 +3,18 @@ import numpy as np
 import librosa
 import glob
 
-def convert_wav_to_npy(filename):
-      if filename.endswith(".wav"):
-          audio, sr = librosa.load(filename)
-          npy_filename = os.path.splitext(filename)[0] + ".npy"
-          np.save(npy_filename, audio)
-          print(f"Converted {filename} to {npy_filename}")
+def convert_wav_to_npy(filepath):
+      if filepath.endswith(".wav"):
+          audio, sr = librosa.load(filepath)
+          # Filepath 에서 .wav 확장자 제외
+          filename = os.path.splitext(filepath)[0]
+          # 'wb' 로 이미 있는 파일의 경우 삭제 후 저장.
+          with open('{}.npy'.format(filename),'wb') as f:
+            np.save(f, audio)
+          print(f"Converted {filepath} to {filename}")
+      else:
+        print(f'{filepath} file does not exist...')
 
 if __name__ == "__main__":
-  for file in glob.glob("**/*.wav",recursive=True):
+  for file in glob.glob("data/voxceleb1/**/*.wav",recursive=True):
     convert_wav_to_npy(file)
