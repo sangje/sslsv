eb6a62bc3c54144cd225294abcd2c11ae26532c6
sangje <parksj2480@naver.com>
2023-02-13 15:42:44 +0900

    Delete Augmentation1111 , save func

--- sslsv/data/AudioDataset1.py
+++ sslsv/data/AudioDataset1.py
@@ -8,6 +8,8 @@ import librosa
 from sslsv.data.AudioAugmentation import AudioAugmentation
 from sslsv.data.utils import read_audio, load_audio
 
+''' 오디오 파일과 프레임길이를 Input
+    프레임길이만큼의 두 오디오를 Return '''
 def sample_frames(audio, frame_length):
     audio_length = audio.shape[1]
     #print(audio_length)
@@ -115,76 +117,4 @@ class AudioDataset(Dataset):
 
         return X, y
 
-
-class AudioDataset111111(Dataset):
-
-    def __init__(self, config):
-        self.config = config
-
-        # Create augmentation module
-        self.wav_augment = None
-        if self.config.wav_augment.enable:
-            self.wav_augment = AudioAugmentation(
-                self.config.wav_augment,
-                self.config.base_path
-            )
-
-        self.load_data()
-
-    def load_data(self):
-        # Create lists of audio paths and labels
-        self.files = []
-        self.labels = []
-        self.nb_classes = 0
-        labels_id = {}
-        for line in open(self.config.train):
-            label, file = line.rstrip().split()
-
-            path = os.path.join(self.config.base_path, file)
-            self.files.append(path)
-
-            if label not in labels_id:
-                labels_id[label] = self.nb_classes
-                self.nb_classes += 1
-            self.labels.append(labels_id[label])
-
-    def __len__(self):
-        if self.config.max_samples: return self.config.max_samples
-        return len(self.labels)
-
-    def preprocess_data(self, data, augment=True):
-        assert data.ndim == 2 and data.shape[0] == 1 # (1, T)
-        if augment and self.wav_augment: data = self.wav_augment(data)        
-        return data
-
-    def __getitem__(self, i):
-
-
-
-        data = read_audio(
-                self.files[i]
-        ) # (1, T)
-        #print("-------------------------------------------")
-
-            
-        #audio = librosa.feature.melspectrogram(y = data, n_mels=64, n_fft=552, win_length = 552, hop_length=100)
-                            
-        return audio.shape
-
-def save(config):
-
-    data_sr = AudioDataset(config)
-
-    for i in range(len(data_sr)):
-        data_sr[i]
-    '''
-    li = []
-
-    data_sr = AudioDataset(config)
-    for i in range(len(data_sr)):
-        srr = data_sr[i]
-        print("mel shape  : " ,srr)
-        #li.append(srr)
-
-    '''
     
